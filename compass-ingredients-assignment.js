8baacf6aab82faa8ad2bdbc507b59ea711c0d896
Nicolas <nchlsreimer@gmail.com>
2022-03-11 13:22:29 -0800

    new file

---
+++ compass-ingredients-assignment.js
@@ -0,0 +1,24 @@
+let ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+// Write a while loop that prints out the contents of ingredients:
+/*
+console.log(ingredients);
+let index = 0;
+while (index < ingredients.length) {
+  console.log(ingredients[index]);
+  index++;
+}
+*/
+
+// Write a for loop that prints out the contents of ingredients:
+/*
+for(index = 0; index < ingredients.length; index++){
+ console.log(ingredients[index]);
+}
+*/
+
+// Write any loop (while or for) that prints out the contents of ingredients backwards:
+for (index = 0; index < ingredients.length; index++) {
+  ingredients = ingredients.reverse()
+}
+console.log(ingredients);
